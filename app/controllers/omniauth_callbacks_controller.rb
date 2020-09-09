bb8a6ad47f95767df977cee2561a798d4a959650
charles-henri poniard <c.poniard@gmail.com>
2020-09-09 17:42:29 +0200

    create CreateCompanyJob, add sidekiq, probleme data for create company

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,20 +2,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def stripe_connect
     auth_data = request.env["omniauth.auth"]
     @user = current_user
-    byebug
     if @user.persisted?
       @user.provider = auth_data.provider
       @user.uid = auth_data.uid
       @user.access_code = auth_data.credentials.token
       @user.publishable_key = auth_data.info.stripe_publishable_key
       @user.save
-
-      # TODO
-      # Ajouter une job pour la création de l'entreprise
-      # require 'stripe'
-      # Stripe.api_key = ENV["STRIPE_PRIVATE_KEY"]
-      # @user = Stripe::Account.retrieve('stripe_accout_id ') ex => acct_1HPS7gJt6BoiLZkS
-      # créer l'entreprise
+      CreateCompanyJob.perform_later(@user)
 
       flash[:notice] = "Stripe Account Created and Connected"
       sign_in_and_redirect @user, event: :authentication
