0f2e1cede562a743030efd66b52f3e32eb37048c
charles-henri poniard <c.poniard@gmail.com>
2020-09-10 11:41:39 +0200

    remove elements

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,17 +1,15 @@
-# $redis = Redis.new
+$redis = Redis.new
 
-# url = ENV["REDIS_URL"]
+url = ENV["REDISCLOUD_URL"]
 
-# if url
-#   Sidekiq.configure_server do |config|
-#     config.redis = { url: url }
-#   end
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
 
-#   Sidekiq.configure_client do |config|
-#     config.redis = { url: url }
-#   end
-#   $redis = Redis.new(:url => url)
-# end
-if ENV["REDIS_URL"]
-    $redis = Redis.new(:url => ENV["REDIS_URL"])
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
+  $redis = Redis.new(:url => url)
 end
+
