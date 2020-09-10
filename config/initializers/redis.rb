b1f11930f106938f24e59f8739a8da0090f6b13f
charles-henri poniard <c.poniard@gmail.com>
2020-09-10 11:23:55 +0200

    new redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,14 +1,17 @@
-$redis = Redis.new
+# $redis = Redis.new
 
-url = ENV["REDIS_URL"]
+# url = ENV["REDIS_URL"]
 
-if url
-  Sidekiq.configure_server do |config|
-    config.redis = { url: url }
-  end
+# if url
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: url }
+#   end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: url }
-  end
-  $redis = Redis.new(:url => url)
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: url }
+#   end
+#   $redis = Redis.new(:url => url)
+# end
+if ENV["REDIS_URL"]
+    $redis = Redis.new(:url => ENV["REDIS_URL"])
 end
