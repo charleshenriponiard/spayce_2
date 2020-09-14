c56e480baba6a339ab508eb332a3e1fa51281c68
charles-henri poniard <c.poniard@gmail.com>
2020-09-14 17:14:28 +0200

    change redirect after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks", registrations: "registrations" }
   root to: 'pages#home'
 
-  get "/users/stripe-connect", to: "pages#stripe_connect", as: "stripe_connect"
+  get "/users/stripe-connect", to: "stripes#sign_up", as: "stripe_connect"
 
   require "sidekiq/web"
   authenticate :user, ->(user) { user.admin? } do
