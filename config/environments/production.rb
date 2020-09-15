48e2f91cd41a8ed92dde39b051d8ea3db18cb673
charles-henri poniard <c.poniard@gmail.com>
2020-09-15 14:51:09 +0200

    config mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "https://spayce2-staging.herokuapp.com/" }
+  config.action_mailer.perform_deliveries = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
