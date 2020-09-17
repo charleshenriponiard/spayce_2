1d3180dc45f3de576adec34e7df6a416bd19d2cb
charles-henri poniard <c.poniard@gmail.com>
2020-09-17 16:10:31 +0200

    connect stripe express

--- app/controllers/registration_steps_controller.rb
+++ app/controllers/registration_steps_controller.rb
@@ -9,8 +9,23 @@ class RegistrationStepsController < ApplicationController
 
   def update
     @user = current_user
-    @user.update(user_params)
-    render_wizard @user
+    account = Stripe::Account.create({
+      country: @user.country,
+      type: 'express',
+      requested_capabilities: ['card_payments', 'transfers'],
+    })
+    @account_links = Stripe::AccountLink.create({
+      account: account["id"],
+      refresh_url: 'http://localhost:5000',
+      return_url: 'http://localhost:5000',
+      type: 'account_onboarding',
+    })
+    object = { uid: account.id }
+    if @user.update(user_params.merge(object))
+      redirect_to @account_links["url"]
+    else
+      render :show
+    end
   end
 
   private
---
+++ app/models/stripe/localcount.rb
@@ -0,0 +1,8 @@
+class Stripe::Localcount
+  include ActiveModel::Model
+  attr_reader :user
+
+  def new(user)
+    byebug
+  end
+end
