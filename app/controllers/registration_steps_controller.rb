126026a0e6cc57cbb8af1c49c2ae0a16b50a069d
charles-henri poniard <c.poniard@gmail.com>
2020-09-17 17:17:39 +0200

    change model name Stripe::Express

--- app/controllers/registration_steps_controller.rb
+++ app/controllers/registration_steps_controller.rb
@@ -9,20 +9,10 @@ class RegistrationStepsController < ApplicationController
 
   def update
     @user = current_user
-    account = Stripe::Account.create({
-      country: @user.country,
-      type: 'express',
-      requested_capabilities: ['card_payments', 'transfers'],
-    })
-    @account_links = Stripe::AccountLink.create({
-      account: account["id"],
-      refresh_url: 'http://localhost:5000',
-      return_url: 'http://localhost:5000',
-      type: 'account_onboarding',
-    })
-    object = { uid: account.id }
+    stripe = Stripe::Express.new
+    object = stripe.sign_up(@user)
     if @user.update(user_params.merge(object))
-      redirect_to @account_links["url"]
+      redirect_to stripe.url
     else
       render :show
     end
@@ -33,5 +23,4 @@ class RegistrationStepsController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :country)
   end
-
 end
--- app/models/stripe/localcount.rb
+++
@@ -1,8 +0,0 @@
-class Stripe::Localcount
-  include ActiveModel::Model
-  attr_reader :user
-
-  def new(user)
-    byebug
-  end
-end
