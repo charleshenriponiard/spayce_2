759baa8cbf6d810b88af11d6ea20b668b32e8cd5
charles-henri poniard <c.poniard@gmail.com>
2020-09-18 14:39:18 +0200

    refacto registrations#edit

--- app/models/stripe/express.rb
+++ app/models/stripe/express.rb
@@ -23,4 +23,9 @@ class Stripe::Express
     answer_request = Stripe::Account.create_login_link(user.uid)
     self.dashboard_url = answer_request["url"]
   end
+
+  def find_account(uid)
+    Stripe::Account.retrieve(uid)
+  end
+
 end
