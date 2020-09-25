a2dc4fbc7b8cff7b2e5ec8c858d28f8eb73d2989
Benoit Calin <benoit.calin@gmail.com>
2020-09-25 16:12:48 +0200

    Add specs on Pundit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,6 +18,10 @@ require 'support/vcr'
 require 'sidekiq/testing'
 Sidekiq::Testing.fake! # by default it is fake
 
+require 'simplecov'
+require "pundit/rspec"
+SimpleCov.start 'rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
