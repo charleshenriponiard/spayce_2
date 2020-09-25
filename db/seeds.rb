cb4997848aba5b84db6a363eab08c3ea368b1673
charles-henri poniard <c.poniard@gmail.com>
2020-09-25 11:25:10 +0200

    Update seed with Project, refacto projects_controller_spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,83 @@
 puts "Destroying everything"
 Company.destroy_all
+Project.destroy_all
 User.destroy_all
 puts "Everything has been destroyed"
 
 puts "Creating things"
 
-puts "Creating Users"
+puts "Creating Users and Projects"
 carlito = User.new(email: 'c.poniard@gmail.com', password: 'secret', admin: true)
 carlito.skip_confirmation!
 carlito.save
+3.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  carlito.projects.create!(
+    name: Faker::Book.title,
+    client_email: "#{ first_name }.#{ last_name }@gmail.com",
+    client_first_name: first_name,
+    client_last_name: last_name,
+    description: Faker::Hipster.paragraph,
+    amount: rand(100..1000),
+    url: Faker::Internet.url,
+    message: Faker::Lorem.paragraph
+  )
+end
+
 benoit = User.new(email: 'benoit.calin@gmail.com', password: 'secret', admin: true)
 benoit.skip_confirmation!
 benoit.save
+
+3.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  benoit.projects.create!(
+    name: Faker::Book.title,
+    client_email: "#{ first_name }.#{ last_name }@gmail.com",
+    client_first_name: first_name,
+    client_last_name: last_name,
+    description: Faker::Hipster.paragraph,
+    amount: rand(100..1000),
+    url: Faker::Internet.url,
+    message: Faker::Lorem.paragraph
+  )
+end
 cedric = User.new(email: 'cedricsauvagetpro@gmail.com', password: 'secret', admin: true)
 cedric.skip_confirmation!
 cedric.save
+3.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  cedric.projects.create!(
+    name: Faker::Book.title,
+    client_email: "#{ first_name }.#{ last_name }@gmail.com",
+    client_first_name: first_name,
+    client_last_name: last_name,
+    description: Faker::Hipster.paragraph,
+    amount: rand(100..1000),
+    url: Faker::Internet.url,
+    message: Faker::Lorem.paragraph
+  )
+end
 fanny = User.new(email: 'fanny@gmail.com', password: 'secret')
 fanny.skip_confirmation!
 fanny.save
-puts "Users creation finished"
+3.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  fanny.projects.create!(
+    name: Faker::Book.title,
+    client_email: "#{ first_name }.#{ last_name }@gmail.com",
+    client_first_name: first_name,
+    client_last_name: last_name,
+    description: Faker::Hipster.paragraph,
+    amount: rand(100..1000),
+    url: Faker::Internet.url,
+    message: Faker::Lorem.paragraph
+  )
+end
+puts "Users and Projects creation finished"
 
 puts "Clear all jobs in mailer queue"
 queue = Sidekiq::Queue.new('mailer')
--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :project do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+
     user
     user_id { user.id }
     name { Faker::Book.title }
     description { Faker::Hipster.paragraph }
     amount { rand(100..1000)}
-    client_email { user.email }
-    client_first_name { user.first_name }
-    client_last_name { user.last_name }
+    client_email { "#{ first_name }.#{ last_name }@gmail.com"}
+    client_first_name { first_name }
+    client_last_name { last_name }
     url { Faker::Internet.url }
     message { Faker::Lorem.paragraph }
   end
