5b236e76aaca82e313d8a40cc0eb8ef6c7d89077
charles-henri poniard <c.poniard@gmail.com>
2020-09-28 17:22:53 +0200

    Project show destroy ok

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -5,11 +5,19 @@ class ProjectPolicy < ApplicationPolicy
     end
   end
 
+  def show?
+    true
+  end
+
   def new?
     create?
   end
 
   def create?
+    true
+  end
+
+  def destroy?
     true
   end
 end
