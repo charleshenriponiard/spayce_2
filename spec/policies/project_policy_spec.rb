a92376dbb5cbdbaf81112ff4347a8dc6c99c95e7
charles-henri poniard <c.poniard@gmail.com>
2020-10-14 11:39:19 +0200

    test ProjectPolicy::Scope

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe ProjectPolicy do
   subject { ProjectPolicy }
 
+  def resolve_for(user)
+    subject::Scope.new(user, Project).resolve
+  end
+
   permissions :new?, :create? do
     it "grants access to any user" do
       expect(subject).to permit(User.new, Project.new)
@@ -18,6 +22,13 @@ RSpec.describe ProjectPolicy do
   permissions :show?, :edit?, :update?, :destroy? do
     it "grants access to any user" do
       expect(subject).to permit(User.new, Project.new)
+    end
+  end
+
+  permissions ".scope" do
+    it "Should return the same number of projects I have" do
+      user = create(:user, email: "test@gmail.com")
+      expect(resolve_for(user).count).to eq(user.projects.count)
     end
   end
 end
