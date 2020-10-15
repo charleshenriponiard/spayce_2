1d3282a9a7bf15cd271877a3046159c483895ad7
charles-henri poniard <c.poniard@gmail.com>
2020-10-15 16:42:13 +0200

    add rspec bill_helper

--- app/helpers/bill_helper.rb
+++ app/helpers/bill_helper.rb
@@ -1,6 +1,5 @@
 module BillHelper
   def bill_details(details)
-    binding.pry
     content_tag :div, class: "bill_details #{details[:class] if details.has_key?(:class)}" do
       content_tag(:span, details[:text]) + content_tag(:span, "$ #{details[:price]}")
     end
--- spec/helpers/bill_helper_spec.rb
+++ spec/helpers/bill_helper_spec.rb
@@ -1,10 +1,10 @@
-describe billHelper do
+require 'rails_helper'
+
+RSpec.describe BillHelper, type: :helper do
   describe "#bill_details" do
     it "should return the right content" do
-      binding.pry
-      answer = bill_details({ text: t('view.project.new.bill_tax'), price: 0.00, class: " bill_detail border-bottom pb-3" })
-
-      helper.bill_details({ text: t('view.project.new.bill_tax'), price: 0.00, class: " bill_detail border-bottom pb-3" }).should eql("My Title")
+      answer = bill_details({ text: t('view.project.new.bill_tax'), price: 0.00, class: "bill_detail border-bottom pb-3" })
+      expect(answer).to eql("<div class=\"bill_details bill_detail border-bottom pb-3\"><span>Tax</span><span>$ 0.0</span></div>")
     end
   end
 end
