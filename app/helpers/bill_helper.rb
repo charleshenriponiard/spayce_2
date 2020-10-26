fe1ccdcec34f1c2f83b0745beb905a58dca6a34b
Benoit Calin <benoit.calin@gmail.com>
2020-10-26 17:03:10 +0100

    Stimulus calculate price real time

--- app/helpers/bill_helper.rb
+++ app/helpers/bill_helper.rb
@@ -1,7 +1,7 @@
 module BillHelper
-  def bill_details(details)
+  def bill_details(details, data_name)
     content_tag :div, class: "bill_details #{details[:class] if details.has_key?(:class)}" do
-      content_tag(:span, details[:text]) + content_tag(:span, "$ #{details[:price]}")
+      content_tag(:span, details[:text]) + content_tag(:span, "#{details[:price]}", data: { target: data_name })
     end
   end
 end
