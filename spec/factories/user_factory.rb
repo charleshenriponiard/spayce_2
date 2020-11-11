2de30f05dd215dd414a8d8169031c62e5ce12408
charles-henri poniard <c.poniard@gmail.com>
2020-11-11 11:08:17 +0100

    Update project_policy_spec, update user factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,9 +4,11 @@ FactoryBot.define do
     password { "secret" }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name  }
-    before :create, &:confirm
-    after :create do |user|
-      create_list :project, 3, user: user
+    trait :with_project do
+      after :create do |user|
+        create_list :project, 3, user: user
+      end
     end
+    before :create, &:confirm
   end
 end
