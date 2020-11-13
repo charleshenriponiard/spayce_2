7654f6253652ce34cb3e7de32eaa6b5b95d72eba
charles-henri poniard <c.poniard@gmail.com>
2020-11-13 17:21:16 +0100

    sent_email, refacto layout email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'c.poniard@gmail.com'
+  default from: 'cedricsauvagetpro@gmail.com'
   layout 'mailer'
+
+  before_action :add_inline_attachment!
+
+  def add_inline_attachment!
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
+  end
 end
