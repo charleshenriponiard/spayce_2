6ef0cd9ead2a9ec572f686f68c1abf4af296c6ac
Benoit Calin <benoit.calin@gmail.com>
2020-11-13 11:13:44 +0100

    test

--- app/javascript/controllers/bill_controller.js
+++ app/javascript/controllers/bill_controller.js
@@ -4,13 +4,12 @@ export default class extends Controller {
   static targets = [ "price", "spayceCommission", "promoCode", "billTax", "total" ]
 
   calculate(event) {
-
     const amount = event.target.value / 1
     const commission = amount * 0.1
     const taxes = commission * 0.2
     const total = amount - commission - taxes
 
-    this.priceTarget.innerHTML = amount.toFixed(2)
+    this.priceTarget.innerHTML = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(amount.toFixed(2))
     this.spayceCommissionTarget.innerHTML = commission.toFixed(2)
     this.billTaxTarget.innerHTML = taxes.toFixed(2)
     this.totalTarget.innerHTML = total.toFixed(2)
