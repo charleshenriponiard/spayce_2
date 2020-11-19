6674f93af9bcd8bd75404391797c6e4aca92c68b
charles-henri poniard <c.poniard@gmail.com>
2020-11-19 10:44:58 +0100

    test fix bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'cedricsauvagetpro@gmail.com'
   layout 'mailer'
+  @user = params[:user]
 
-  def kyc_validated(user)
-    @user = user
+  def kyc_validated
     mail(to: @user.email, subject: 'Congratulation your KYC is valide !')
   end
 end
