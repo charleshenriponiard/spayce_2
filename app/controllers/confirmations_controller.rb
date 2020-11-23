b93453e332829d0bb0ce41f9a576b91dd62e3258
Benoit Calin <benoit.calin@gmail.com>
2020-11-23 11:51:47 +0100

    Add page / route / redirection for after inactive signup

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,10 @@
 class ConfirmationsController < Devise::ConfirmationsController
   protected
 
+  def before_confirmation_path_for(resource_name, resource)
+    account_confirmation_path
+  end
+
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
     registration_steps_path
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,10 @@ class RegistrationsController < Devise::RegistrationsController
     registration_steps_path
   end
 
+  def after_inactive_sign_up_path_for(resource)
+    account_confirmation_path
+  end
+
   def after_update_path_for(resource)
     root_path
   end
