0693930eff1af46a224aa13c91d8738c9714a708
charles-henri poniard <c.poniard@gmail.com>
2020-11-26 11:04:48 +0100

    resolve confict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_11_25_161642) do
-=======
-ActiveRecord::Schema.define(version: 2020_11_24_152222) do
->>>>>>> 3e19c791d973435b42256007ac70063875b4eaa0
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -86,13 +82,10 @@ ActiveRecord::Schema.define(version: 2020_11_24_152222) do
     t.string "payment_intent_id"
     t.string "slug"
     t.float "discount", default: 0.0
-<<<<<<< HEAD
     t.float "spayce_commission", default: 0.0
     t.float "tax", default: 0.0
     t.float "total", default: 0.0
-=======
     t.string "zipped_key"
->>>>>>> 3e19c791d973435b42256007ac70063875b4eaa0
     t.index ["slug"], name: "index_projects_on_slug", unique: true
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
