f0ced82fe1aa8f4d59a0db0ca4ddd43f89464fa3
charles-henri poniard <c.poniard@gmail.com>
2020-11-30 16:42:54 +0100

    refacto

--- spec/controllers/invoices_controller_spec.rb
+++ spec/controllers/invoices_controller_spec.rb
@@ -4,16 +4,15 @@ require_relative "../support/devise"
 RSpec.describe InvoicesController, type: :controller do
   let(:user) { create(:user, :with_project) }
   let(:project) { user.projects.last }
+  let(:invoice) { Invoice.create(project: project) }
 
   describe "#show" do
     it "test invoice view" do
-      invoice = Invoice.create(project: project)
       get :show, params: { project_slug: project.slug, id: invoice.id }
       assert_template :show
     end
 
     it "should return the correct layout" do
-      invoice = Invoice.create(project: project)
       get :show, params: { project_slug: project.slug, id: invoice.id }
       expect(subject).to render_template("layouts/invoice")
     end
