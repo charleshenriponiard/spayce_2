eb931e5f6cc1a69f0b1d545c7f511137fb83decb
charles-henri poniard <c.poniard@gmail.com>
2020-11-30 15:56:11 +0100

    fix bug ?

--- spec/jobs/update_project_job_spec.rb
+++ spec/jobs/update_project_job_spec.rb
@@ -5,39 +5,41 @@ Sidekiq::Testing.inline!
 RSpec.describe UpdateProjectJob, type: :job do
   let(:user) { create(:user, :with_project) }
   let(:project) { user.projects.last }
-
-  before(:each) do
-    hash = { payment_status: "payment_succeeded",
-                status: "paid",
-                tax: project.tax,
-                spayce_commission: project.commission,
-                total: project.total
-              }
-    UpdateProjectJob.perform_now(project, hash)
-  end
+  let(:hash) { {  payment_status: "payment_succeeded",
+              status: "paid",
+              tax: project.tax,
+              spayce_commission: project.commission,
+              total: project.total
+            } }
 
   describe "#perfom" do
     it "change payment_status to payment_succeeded" do
+      UpdateProjectJob.perform_now(project, hash)
       expect(project.payment_succeeded?).to be true
     end
 
     it "change status to paid" do
+      UpdateProjectJob.perform_now(project, hash)
       expect(project.paid?).to be true
     end
 
     it "should add commission spayce" do
+      UpdateProjectJob.perform_now(project, hash)
       expect(project.spayce_commission).to eq(100.0)
     end
 
     it "should add tax" do
+      UpdateProjectJob.perform_now(project, hash)
       expect(project.tax).to be_a(Money)
     end
 
     it "should add total" do
+      UpdateProjectJob.perform_now(project, hash)
       expect(project.total).to be_a(Money)
     end
 
     it "Should create has one invoice" do
+      UpdateProjectJob.perform_now(project, hash)
       expect(project.invoice.project_id).to eq(project.id)
     end
   end
