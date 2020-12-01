05844eaf5e4a88add0368c9ed1c86b1c9c23e0a5
Benoit Calin <benoit.calin@gmail.com>
2020-12-01 17:14:30 +0100

    I18n for contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     if @message.valid?
       MessageMailer.contact_me(@message, current_user).deliver_now
-      redirect_to root_path, notice: "Nous avons bien reçu votre message, merci !"
+      redirect_to root_path, notice: t('controllers.messages.sent')
     else
       render :new
     end
