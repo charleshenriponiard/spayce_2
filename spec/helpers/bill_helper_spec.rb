3acd59b6138243a16f0acbac630f57fb5eb7b6f3
Benoit Calin <benoit.calin@gmail.com>
2020-12-01 11:54:15 +0100

    Correct helper test

--- spec/helpers/bill_helper_spec.rb
+++ spec/helpers/bill_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe BillHelper, type: :helper do
   describe "#bill_details" do
     it "should return the right content" do
-      answer = bill_details({ text: t('views.projects.new.bill_tax'), price: 0.00, class: "bill_detail border-bottom pb-3" }, "bill.details")
+      answer = bill_details({ text: t('views.projects.bill.tax'), price: 0.00, class: "bill_detail border-bottom pb-3" }, "bill.details")
       expect(answer).to eql("<div class=\"bill_details bill_detail border-bottom pb-3\"><span>Tax</span><span data-target=\"bill.details\">$0.00</span></div>")
     end
   end
