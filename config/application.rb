37892f563ff06c63e0d45b84df9c42101e6562e3
charles-henri poniard <c.poniard@gmail.com>
2020-12-21 09:50:20 +0100

    in progresse

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Spayce2
     # active storage
     config.active_storage.replace_on_assign_to_many = false
     config.i18n.available_locales = [:en, :fr]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :fr
   end
 end
